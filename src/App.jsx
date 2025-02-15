3e173baefcb8de3d993cffc0cd7673859c15e91d
hsiu chen <166912934+hsiu2701@users.noreply.github.com>
2025-02-15 16:37:07 +0800

    嘗試移除新增圖片

--- src/App.jsx
+++ src/App.jsx
@@ -159,27 +159,27 @@ function App() {
     });
   };
   // 新增圖片
-  const addImg = () => {
-    const newImages = [...tempProduct.imagesUrl, ""];
+  // const addImg = () => {
+  //   const newImages = [...tempProduct.imagesUrl, ""];
 
-    // newImages.push("");
+  //   // newImages.push("");
 
-    setTempProduct({
-      ...tempProduct,
-      imagesUrl: newImages,
-    });
-  };
-  // 刪除圖片
-  const deleteImg = () => {
-    const newImages = [...tempProduct.imagesUrl];
+  //   setTempProduct({
+  //     ...tempProduct,
+  //     imagesUrl: newImages,
+  //   });
+  // };
+  // // 刪除圖片
+  // const deleteImg = () => {
+  //   const newImages = [...tempProduct.imagesUrl];
 
-    newImages.pop();
+  //   newImages.pop();
 
-    setTempProduct({
-      ...tempProduct,
-      imagesUrl: newImages,
-    });
-  };
+  //   setTempProduct({
+  //     ...tempProduct,
+  //     imagesUrl: newImages,
+  //   });
+  // };
   // 新增產品,注意有data,數字要轉型
   const createProduct = async () => {
     try {
